f4be9a23dfd824e62402675fe121ca83234a4495
Lakshaya Singh Tanwar <tanwarlakshay21@gmail.com>
2025-07-08 20:08:20 +0530

    INITIAL COMMIT

---
+++ XylophoneTests/XylophoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneTests.swift
+//  XylophoneTests
+//
+//  Created by Lakshaya Singh Tanwar on 30/06/25.
+//
+
+import Testing
+@testable import Xylophone
+
+struct XylophoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
